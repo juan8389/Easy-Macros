56650e2d9397e7bc252ac1eb39a20194e749fe90
JuanDesktop <juan8389@gmail.com>
2021-04-03 12:26:16 -0400

    feat:adding image from the api

--- public/js/api.js
+++ public/js/api.js
@@ -1,57 +1,79 @@
 $(document).ready(function () {
 
-    
+
     // var searchButton = document.getElementById("search_recipe")
-  
+
     function searchRecipe(query) {
         // console.log("retriving recipes")
         // API Edeman \
-  
-          var appID = "6775564b";
-          var appKey = "5bd1cfdc845fb142ed758081972100c7";
-          var queryURL = `https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${appKey}`;
-  
-          $.ajax({
+
+        var appID = "6775564b";
+        var appKey = "5bd1cfdc845fb142ed758081972100c7";
+        var queryURL = `https://api.edamam.com/search?q=${query}&app_id=${appID}&app_key=${appKey}`;
+
+        $.ajax({
             url: queryURL,
             crossDomain: true,
             method: "GET",
             xhrFields: {
-              withCredentials: true
+                withCredentials: true
             }
-          }).then(function (response) {
-            console.log(response); 
+        }).then(function (response) {
+            console.log(response);
             console.log(response.hits[0].recipe.image);
             console.log(response.hits[0].recipe.url);
-           
-  
-           //not sure how to retrieve date from a loop. when i loop within the loop ¯\_(ツ)_/¯ git add
+
+
+            //not sure how to retrieve date from a loop. when i loop within the loop ¯\_(ツ)_/¯ git add
             if (response) {
-              for (var i=0; i < 3; i++) {
-                console.log(response.hits[i].recipe.digest[i].total);
+                for (var i = 0; i < 3; i++) {
+                    console.log(response.hits[i].recipe.digest[i].total);
+
+                }
+                $("#recipe-macros").html(
+                    "<b>Macros: <b/>" + "" + (Math.round(response.hits[i].recipe.digest[0].total)) + "g Fats<b/>" + " " +
+                    (Math.round(response.hits[i].recipe.digest[1].total)) + "g Carbs<b/>" + " " +
+                    (Math.round(response.hits[i].recipe.digest[2].total)) + "g Proteins<b/>"
+                );
+
+
+                response.hits.forEach(function(item, index){
+                var image =`<img src="${item.recipe.image}" />`
+                $("#img_macros").append(image)
+                    console.log(item.recipe.image)
+
+                })
+
+
+
+
+
+
+
+
+
+
+
+
             }
-            }
-           $("#recipe-macros").html(
-             "<b>Macros: <b/>" + "" + (Math.round(response.hits[i].recipe.digest[0].total)) + "g Fats<b/>" + " " +
-             (Math.round(response.hits[i].recipe.digest[1].total)) + "g Carbs<b/>" + " " +
-             (Math.round(response.hits[i].recipe.digest[2].total)) + "g Proteins<b/>"
-             ); 
-  
-          
-   
-         
-  });
-         
-          
-  
+            // $("#img_macros").html(
+            //     "<b><b/>" + "" + (response.hits[0].recipe.image) + (response.hits[1].recipe.image) + (response.hits[2].recipe.image) + (response.hits[3].recipe.image) + (response.hits[4].recipe.image) + (response.hits[5].recipe.image) + (response.hits[6].recipe.image));
+
+
+
+        });
+
+
+
     }
-  
+
     $("#submit").on("click", function (event) {
         event.preventDefault();
         var query = $("#search_recipe").val();
         console.log(query)
         searchRecipe(query)
-        
-  
+
+
     })
-  })
-  
+})
+
